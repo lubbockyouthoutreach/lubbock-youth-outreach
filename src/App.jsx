678fdc34218a30a9d9f34f74611d35a9a6290d8e
NilsG-S <nils.griswold.steiner@gmail.com>
2017-10-02 02:01:52 -0500

    Fixed no-scroll problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HashRouter, Route, Redirect } from 'react-router-dom';
 
 import GrommetApp from 'grommet/components/App';
-import Box from 'grommet/components/Box';
 
 import Nav from 'components/Nav.jsx';
 import Home from 'components/home/Home.jsx';
@@ -16,24 +15,22 @@ const App = () => (
       centered={false}
       inline={false}
     >
-      <Box full>
-        <Nav />
+      <Nav />
 
-        {/* Load the home page by default */}
-        <Route
-          exact
-          path='/'
-          render={() => (
-            <Redirect to='/home' />
-          )}
-        />
+      {/* Load the home page by default */}
+      <Route
+        exact
+        path='/'
+        render={() => (
+          <Redirect to='/home' />
+        )}
+      />
 
-        {/* Set the children of the primary component */}
-        <Route path='/home' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/get-involved' component={GetInvolved} />
-        <Route path='/parents' component={Parents} />
-      </Box>
+      {/* Set the children of the primary component */}
+      <Route path='/home' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/get-involved' component={GetInvolved} />
+      <Route path='/parents' component={Parents} />
     </GrommetApp>
   </HashRouter>
 );
