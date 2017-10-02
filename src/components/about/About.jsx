6489f123bfbbe97f54c904e58c308432f1e89ce3
NilsG-S <nils.griswold.steiner@gmail.com>
2017-10-01 19:08:31 -0500

    Added missing components

---
+++ src/components/about/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Paragraph from 'grommet/components/Paragraph';
+import Box from 'grommet/components/Box';
+
+function About() {
+  return (
+    <Box align='start' margin='small' pad='large'>
+      About LYO
+      <Paragraph>
+        Stuff about LYO
+      </Paragraph>
+    </Box>
+  );
+}
+
+export default About;
---
+++ src/components/get-involved/GetInvolved.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Paragraph from 'grommet/components/Paragraph';
+import Box from 'grommet/components/Box';
+
+function GetInvolved() {
+  return (
+    <Box align='start' margin='small' pad='large'>
+      Get Involved
+      <Paragraph>
+        Stuff about getting involved.
+      </Paragraph>
+    </Box>
+  );
+}
+
+export default GetInvolved;
