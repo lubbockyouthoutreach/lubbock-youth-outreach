2c8698e2e7a3cba7fad002b90ad0c5ce6a30546a
NilsG-S <nils.griswold.steiner@gmail.com>
2017-10-01 20:36:07 -0500

    Got Joining section properly formatted

--- src/components/get-involved/Joining.jsx
+++ src/components/get-involved/Joining.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 
-import Paragraph from 'grommet/components/Paragraph';
 import Box from 'grommet/components/Box';
+import Sidebar from 'grommet/components/Sidebar';
+import Heading from 'grommet/components/Heading';
+import Paragraph from 'grommet/components/Paragraph';
+import Image from 'grommet/components/Image';
+
+import GivingHandsPavillion from './wbi-giving-hands-pavillion.png';
 
 function Joining() {
   return (
-    <Box align='start' margin='small' pad='large'>
-      How to Join
-      <Paragraph>
-        Joining is simple, do zyx and say it six times fast.
-      </Paragraph>
+    <Box
+      flex
+      pad='small'
+      direction='row'
+      responsive={false}
+    >
+      <Box
+        flex
+        direction='column'
+        responsive={false}
+      >
+        <Heading tag='h2'>
+          How to get involved
+        </Heading>
+        <Paragraph>
+          Joining is simple, do zyx and say it six times fast.
+        </Paragraph>
+      </Box>
+      <Sidebar
+        fixed={false}
+        full={false}
+        size='medium'
+      >
+        <Image
+          width='100%'
+          height='100%'
+          size='medium'
+          src={`${GivingHandsPavillion}`}
+        />
+      </Sidebar>
     </Box>
   );
 }
